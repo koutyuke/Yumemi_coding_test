bf5d5896cc2336b84289b1765b05f9af39c17352
koutyuke <gunji.kousuke08@gmail.com>
2024-05-27 00:55:25 +0900

    feat: ✨ (app) サイトのメタ情報の作成

---
+++ src/app/robot.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from "next";
+import { baseUrl } from "@/shared/const/base";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/api", "/_next"],
+    },
+    sitemap: new URL("/sitemap.xml", baseUrl).href,
+  };
+}
---
+++ src/app/satemap.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+import { baseUrl } from "@/shared/const/base";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: new URL("/", baseUrl).href,
+      lastModified: new Date(),
+    },
+  ];
+}
